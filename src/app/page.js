2acd34ab84b5fdae364abd915f76c2f3ae8bb930
joaquinnl22 <jneiralasala@gmail.com>
2025-01-16 13:48:00 -0300

    feat: fix

--- src/app/page.js
+++ src/app/page.js
@@ -13,8 +13,6 @@ import {
 import { ImOffice } from "react-icons/im";
 import { PiShippingContainerFill } from "react-icons/pi";
 
-
-
 export default function Home() {
   const [items, setItems] = useState({
     baños: [],
@@ -27,24 +25,24 @@ export default function Home() {
     guardias: [],
     reef: [],
   });
-  const [loading, setLoading] = useState(false); 
+  const [loading, setLoading] = useState(false);
 
   const [selectedType, setSelectedType] = useState("");
   const [isAgregarOpen, setIsAgregarOpen] = useState(false);
   const handleCloseAgregar = () => setIsAgregarOpen(false);
 
   const fetchItems = async () => {
-    setLoading(true); 
+    setLoading(true);
     try {
       const response = await fetch("/api/getItems");
       const data = await response.json();
-  
+
       const groupedItems = data.reduce((acc, item) => {
         if (!acc[item.tipo]) acc[item.tipo] = [];
         acc[item.tipo].push(item);
         return acc;
       }, {});
-  
+
       setItems({
         baños: groupedItems.baño || [],
         BOD20: groupedItems.BOD20 || [],
@@ -59,10 +57,9 @@ export default function Home() {
     } catch (error) {
       console.error("Error fetching items:", error);
     } finally {
-      setLoading(false); 
+      setLoading(false);
     }
   };
-  
 
   const handleOpenAgregar = () => setIsAgregarOpen(true);
 
@@ -147,89 +144,121 @@ export default function Home() {
 
   const globalStock = calculateGlobalStock(items);
 
-  
-    return (
-      <div className="min-h-screen bg-gray-100 p-4 sm:p-6 lg:p-8">
-        {/* Spinner durante la carga */}
-        {loading ? (
-          <div className="flex justify-center items-center min-h-[50vh]">
-             <PiShippingContainerFill className="text-black text-4xl animate-spin" />
-          </div>
-        ) : (
-          <>
-            {/* Contenido principal */}
-            <Navbar onAddClick={handleOpenAgregar} onFilterChange={setSelectedType} />
-    
-            <ModalAgregar
-              isOpen={isAgregarOpen}
-              onClose={handleCloseAgregar}
-              onSave={async (data) => {
-                try {
-                  const formData = new FormData();
-                  for (const key in data) {
-                    formData.append(key, data[key]);
-                  }
-    
-                  const response = await fetch("/api/addItem", {
-                    method: "POST",
-                    body: formData,
-                  });
-    
-                  if (!response.ok) throw new Error("Failed to add item");
-    
-                  const result = await response.json();
-                  const newItem = result.data;
-    
-                  setItems((prev) => ({
-                    ...prev,
-                    [newItem.tipo]: [...(prev[newItem.tipo] || []), newItem],
-                  }));
-    
-                  handleCloseAgregar();
-                } catch (error) {
-                  console.error(error);
+  return (
+    <div className="min-h-screen bg-gray-100 p-4 sm:p-6 lg:p-8">
+      {/* Spinner durante la carga */}
+      {loading ? (
+        <div className="flex justify-center items-center min-h-[50vh]">
+          <PiShippingContainerFill className="text-black text-4xl animate-spin" />
+        </div>
+      ) : (
+        <>
+          {/* Contenido principal */}
+          <Navbar
+            onAddClick={handleOpenAgregar}
+            onFilterChange={setSelectedType}
+          />
+
+          <ModalAgregar
+            isOpen={isAgregarOpen}
+            onClose={handleCloseAgregar}
+            onSave={async (data) => {
+              try {
+                const formData = new FormData();
+                for (const key in data) {
+                  formData.append(key, data[key]);
                 }
-              }}
-            />
-    
-            <h1 className="text-2xl sm:text-3xl lg:text-4xl font-extrabold text-center text-gray-800 my-6">
-              Stock de los container
-            </h1>
-    
-            {/* Resumen Global */}
-            <div className="bg-white rounded-xl shadow-md p-4 transition-transform transform hover:scale-105 hover:shadow-lg flex flex-col items-center mb-6">
-              <h2 className="text-xl sm:text-2xl font-bold text-gray-800">Resumen Global</h2>
-              <div className="grid grid-cols-3 gap-4 w-full text-center">
-                <div>
-                  <h3 className="text-lg sm:text-xl font-bold text-gray-600">
-                    Disponible para arriendo
-                  </h3>
-                  <div className="text-2xl font-extrabold text-green-600">{globalAvailable}</div>
+
+                const response = await fetch("/api/addItem", {
+                  method: "POST",
+                  body: formData,
+                });
+
+                if (!response.ok) throw new Error("Failed to add item");
+
+                const result = await response.json();
+                const newItem = result.data;
+
+                setItems((prev) => ({
+                  ...prev,
+                  [newItem.tipo]: [...(prev[newItem.tipo] || []), newItem],
+                }));
+
+                handleCloseAgregar();
+              } catch (error) {
+                console.error(error);
+              }
+            }}
+          />
+
+          <h1 className="text-2xl sm:text-3xl lg:text-4xl font-extrabold text-center text-gray-800 my-6">
+            Stock de los container
+          </h1>
+
+          {/* Resumen Global */}
+          <div className="bg-white rounded-xl shadow-md p-4 transition-transform transform hover:scale-105 hover:shadow-lg flex flex-col items-center mb-6">
+            <h2 className="text-xl sm:text-2xl font-bold text-gray-800">
+              Resumen Global
+            </h2>
+            <div className="grid grid-cols-3 gap-4 w-full text-center">
+              <div>
+                <h3 className="text-lg sm:text-xl font-bold text-gray-600">
+                  Disponible para arriendo
+                </h3>
+                <div className="text-2xl font-extrabold text-green-600">
+                  {globalAvailable}
                 </div>
-                <div>
-                  <h3 className="text-lg sm:text-xl font-bold text-gray-600">Arriendado </h3>
-                  <div className="text-2xl font-extrabold text-red-500">{globalOccupied}</div>
+              </div>
+              <div>
+                <h3 className="text-lg sm:text-xl font-bold text-gray-600">
+                  Arriendado{" "}
+                </h3>
+                <div className="text-2xl font-extrabold text-red-500">
+                  {globalOccupied}
                 </div>
-                <div>
-                  <h3 className="text-lg sm:text-xl font-bold text-gray-600">Stock Total</h3>
-                  <div className="text-2xl font-extrabold text-blue-500">{globalStock}</div>
+              </div>
+              <div>
+                <h3 className="text-lg sm:text-xl font-bold text-gray-600">
+                  Stock Total
+                </h3>
+                <div className="text-2xl font-extrabold text-blue-500">
+                  {globalStock}
                 </div>
               </div>
             </div>
-    
-            <div className="grid grid-rows-8 gap-4">
-              {renderBlock("Baños", items.baños, FaToilet)}
-              {renderBlock("BOD20", items.BOD20, FaWarehouse)}
-              {renderBlock("BOD40", items.BOD40, FaWarehouse)}
-              {renderBlock("Oficinas", items.oficinas, FaBuilding)}
-              {renderBlock("Oficinas con baño", items.oficinasconbaño, ImOffice)}
-              {renderBlock("Comedores", items.comedores, FaUtensils)}
-              {renderBlock("Camarines", items.camarines, FaUsers)}
-              {renderBlock("Guardias", items.guardias, FaShieldAlt)}
-              {renderBlock("Reef", items.reef, PiShippingContainerFill)}
+          </div>
+          <div className="bg-white rounded-xl shadow-md p-4 transition-transform transform hover:scale-105 hover:shadow-lg flex flex-col items-center mb-6">
+            <div className="grid grid-cols-5 gap-4 w-full text-center">
+              <h3 className="text-lg sm:text-xl font-bold text-gray-800">
+                Items
+              </h3>
+              <h3 className="text-lg sm:text-xl font-bold text-gray-800">
+                Disponible para arriendo
+              </h3>
+              <h3 className="text-lg sm:text-xl font-bold text-gray-800">
+                EN Mantención
+              </h3>
+              <h3 className="text-lg sm:text-xl font-bold text-gray-800">
+                Arrendados
+              </h3>
+              <h3 className="text-lg sm:text-xl font-bold text-gray-800">
+                Stock Total
+              </h3>
             </div>
-          </>
-        )}
-      </div>
-    );
-  }
+          </div>
+          <div className="grid grid-rows-8 gap-4">
+            {renderBlock("Baños", items.baños, FaToilet)}
+            {renderBlock("BOD20", items.BOD20, FaWarehouse)}
+            {renderBlock("BOD40", items.BOD40, FaWarehouse)}
+            {renderBlock("Oficinas", items.oficinas, FaBuilding)}
+            {renderBlock("Oficinas con baño", items.oficinasconbaño, ImOffice)}
+            {renderBlock("Comedores", items.comedores, FaUtensils)}
+            {renderBlock("Camarines", items.camarines, FaUsers)}
+            {renderBlock("Guardias", items.guardias, FaShieldAlt)}
+            {renderBlock("Reef", items.reef, PiShippingContainerFill)}
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
