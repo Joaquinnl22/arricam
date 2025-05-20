f07149e0cffa0a2943e6b8edefaa2dfff18d468a
Joaquinl <96803068+Joaquinnl22@users.noreply.github.com>
2025-05-20 02:10:49 +0000

    feat: fix get day

--- src/app/api/get-latest-summary/route.js
+++ src/app/api/get-latest-summary/route.js
@@ -1,26 +1,29 @@
+export const dynamic = "force-dynamic";
+
 import connectToDatabase from "@/lib/mongodb";
 import GlobalSummary from "@/models/GlobalSummary";
-
-export const dynamic = "force-dynamic";
 
 export async function GET() {
   try {
     await connectToDatabase();
 
-    const summaries = await GlobalSummary.find({})
+    const todayISO = new Date().toISOString().split("T")[0];
+
+    // Excluir la fecha de hoy en la búsqueda
+    const summaries = await GlobalSummary.find({ date: { $lt: todayISO } })
       .sort({ date: -1 })
-      .limit(2)
+      .limit(1)
       .lean();
 
-    if (summaries.length < 2) {
+    if (!summaries.length) {
       return new Response(
-        JSON.stringify({ message: "No hay suficientes resúmenes disponibles" }),
+        JSON.stringify({ message: "No hay resúmenes anteriores a hoy" }),
         { status: 404 }
       );
     }
 
     return new Response(
-      JSON.stringify({ date: summaries[1].date }),
+      JSON.stringify({ date: summaries[0].date }),
       { status: 200, headers: { "Content-Type": "application/json" } }
     );
   } catch (error) {
